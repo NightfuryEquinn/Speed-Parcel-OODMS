7830834bc202e23d7e03a2d92ed74cf264c41a8f
unknown <NightfuryEquinn>
2023-01-17 17:11:38 +0800

    Added main color file

--- oodms/src/oodms/admin/login.java
+++ oodms/src/oodms/admin/login.java
@@ -1,8 +1,12 @@
 package oodms.admin;
 
+import style.Palette;
+
 public class login {
     public static void main(String[] args) {
         
+        Palette test = new Palette();
+        
+        System.out.println(test.darkBrown);
     }
-    
 }
