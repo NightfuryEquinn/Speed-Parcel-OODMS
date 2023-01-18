1cef00f29aea5ce5bdc3994bcdbff9c24e4dc1d1
unknown <NightfuryEquinn>
2023-01-18 10:40:07 +0800

    Updated Java to JFrame

--- oodms/src/style/Palette.java
+++
@@ -1,9 +0,0 @@
-package style;
-
-public class Palette {
-    // Colors
-    public String darkBrown = "#4C2B18";
-    public String lightBrown = "#B89168";
-    public String lightRed = "#E6CFC9";
-    public String lightYellow = "#FAF2E0";
-}
