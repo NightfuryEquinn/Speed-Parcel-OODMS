4a4450d094c025e9cd4a90e9a62314fc0fe2d2f0
unknown <NightfuryEquinn>
2023-01-31 16:51:24 +0800

    Bug fixes

--- oodms/src/oodms/oop/encapsulation_welcomePage.java
+++ oodms/src/oodms/oop/encapsulation_welcomePage.java
@@ -4,23 +4,32 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import oodms.general.WelcomePage;
+import java.io.LineNumberReader;
+import java.util.Arrays;
 
 public class encapsulation_welcomePage {
 
     public static void main(String[] args) {
         try {
-            InputStream dataFile = WelcomePage.class.getResourceAsStream("/oodms/database/credentialsData.txt");
-
+            // Get file from another package
+            InputStream dataFile = encapsulation_welcomePage.class.getResourceAsStream("/oodms/database/credentialsData.txt");
+            
+            // Read file
             BufferedReader br = new BufferedReader(new InputStreamReader(dataFile));
+            
+            // Get Number of Lines in File
+            LineNumberReader lnr = new LineNumberReader(new InputStreamReader(dataFile));
+            
+            int lineLength = lnr.getLineNumber();
 
+            // Create Array in Array
             String credentials;
-
+            
             credentials = br.readLine();
             
-            String[] credentialsArr = credentials.split("\\s\\|\\s");
-
-            System.out.println(credentialsArr);
+            String[] credentialsData = credentials.split("\\s\\|\\s");
+            
+            String[][] credentialsArr = new String[lineLength][2];
         }
         
         catch(IOException e) {
